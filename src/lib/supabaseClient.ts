7323e1af7a253c963ae2c6a7df9cd2668846086f
RuntyAxis1111 <johan.martinez@hybecorp.com>
2025-07-23 15:38:06 -0600

    Corregir credenciales de Supabase

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = 'https://hqrobbmdvanuozzhjdun.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4cWtxdmhxZmJwYnBrZ3RhaHp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTkzNDI5NzEsImV4cCI6MjAzNDkxODk3MX0.YtNhNGkzb7nEoSh6kEpKzqx7VQoUzuoIkzjLjqzqzqI';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imhxcm9iYm1kdmFudW96emhqZHVuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTA4MjY0ODgsImV4cCI6MjA2NjQwMjQ4OH0.Pv6RDwe1-1rlxDPdEw-hD_kuxRDQsEwG4MK41QSzTdc';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
